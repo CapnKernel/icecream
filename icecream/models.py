b6b73daa7bd1bbfd0054cc37aa2ec00235916598
Mitch Davis <mjd@afork.com>
2013-02-25 13:12:50 +1100

    patch icecream-model

--- icecream/models.py
+++ icecream/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Flavour(models.Model):
+    name = models.CharField(max_length=40)
+    litres = models.FloatField() # How many litres we have in the store
+    sellprice = models.DecimalField(decimal_places=2, max_digits=5) # How much it costs per litre
+
+    def __unicode__(self):
+        return self.name
+
