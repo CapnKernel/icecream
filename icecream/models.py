968875a7654ff58327ea99b498cf98ce2314fa6d
Mitch Davis <mjd@afork.com>
2013-02-25 13:12:50 +1100

    patch field-help

--- icecream/models.py
+++ icecream/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 class Flavour(models.Model):
     name = models.CharField(max_length=40)
-    litres = models.FloatField() # How many litres we have in the store
-    sellprice = models.DecimalField(decimal_places=2, max_digits=5) # How much it costs per litre
+    litres = models.FloatField(help_text="How many litres do we have in the store?") 
+    sellprice = models.DecimalField(decimal_places=2, max_digits=5, help_text="How much per litre?") # 
 
     def __unicode__(self):
         return self.name
