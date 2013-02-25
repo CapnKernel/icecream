105992e74d8d48e97a7caf7170e1195b39e958cb
Mitch Davis <mjd@afork.com>
2013-02-25 13:12:50 +1100

    patch edit

--- icecream/views.py
+++ icecream/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render_to_response, get_object_or_404
-from django.shortcuts import Http404
 from django.template import RequestContext
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
@@ -24,7 +23,16 @@ def flavour_add(request):
     return render_to_response('icecream/flavour-add.html', {}, context_instance=RequestContext(request))
 
 def flavour_edit(request, id):
-    raise Http404
+    flavour = get_object_or_404(Flavour, pk=id)
+    if request.method == 'POST':
+        flavour.name = request.POST['name']
+        flavour.litres = request.POST['litres']
+        flavour.sellprice = request.POST['sellprice']
+        flavour.save()
+
+        return HttpResponseRedirect(reverse('icecream.views.flavours'))
+
+    return render_to_response('icecream/flavour-edit.html', {'flavour': flavour}, context_instance=RequestContext(request))
 
 def flavour_delete(request, id):
     # GET: Prompt for whether to delete a flavour
